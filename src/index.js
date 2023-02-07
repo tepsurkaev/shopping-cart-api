4cd12203cd8c5a79a068b22514425cd22721190e
tepsurkaev <unknownsayid@mail.ru>
2023-02-08 01:20:33 +0300

    Инициализация проекта, настройка линтера и форматтера

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+
+mongoose.set('strictQuery', true);
+
+const app = express();
+
+const { PORT, DB_URI } = process.env;
+
+app.use(express.json());
+app.use(cors());
+
+(async function () {
+  try {
+    await mongoose.connect(DB_URI);
+
+    app.listen(PORT, () => {
+      // eslint-disable-next-line no-console
+      console.log(`Успешное соединение: Порт ${PORT}`);
+    });
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.log(`Ошибка при подключении: ${e}`);
+  }
+})();
